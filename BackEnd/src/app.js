ba56f86916828bdf8ef783966058607e05d66895
Sushil Prajapati <70131939+Sushil811@users.noreply.github.com>
2025-11-13 07:25:48 +0530

    Update app.js

--- BackEnd/src/app.js
+++ BackEnd/src/app.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const aiRoutes = require('./routes/ai.routes')
-const cors = require('cors')
-const path = require('path')
-const app = express();
+//const express = require('express');
+// const aiRoutes = require('./routes/ai.routes')
+// const cors = require('cors')
+// const path = require('path')
+// const app = express();
 
-const _dirname = path.resolve()
+// const _dirname = path.resolve()
 
-app.use(cors())
+// app.use(cors())
 
-app.use(express.json()) //if we know this line we'll get an error(undefined body)
-app.use(express.static(path.join(_dirname, '/Frontend/dist')));
-app.get('*', (_, res)=>{
-    res.sendFile(path.resolve(_dirname, 'Frontend', 'dist', 'index.html'))
-})
+// app.use(express.json()) //if we know this line we'll get an error(undefined body)
+// app.use(express.static(path.join(_dirname, '/Frontend/dist')));
+// app.get('*', (_, res)=>{
+//     res.sendFile(path.resolve(_dirname, 'Frontend', 'dist', 'index.html'))
+// })
 
-app.get('/', (req,res)=>{
-    res.send('Hello World')
-})
+// app.get('/', (req,res)=>{
+//     res.send('Hello World')
+// })
 
-app.use('/ai', aiRoutes);
+// app.use('/ai', aiRoutes);
 
-module.exports = app
+// module.exports = app
