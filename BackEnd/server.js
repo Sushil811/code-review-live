c0ced07a3fc8b6f19c081e3b4d4284fd9e12beba
Sushil Prajapati <70131939+Sushil811@users.noreply.github.com>
2025-11-13 07:26:24 +0530

    Set up Express server with AI routes and CORS
    Refactored server setup to use Express and added routes.

--- BackEnd/server.js
+++ BackEnd/server.js
@@ -1,7 +1,30 @@
 require('dotenv').config()
-const app = require('./src/app')
+//const app = require('./src/app')
+
+const express = require('express');
+const aiRoutes = require('./routes/ai.routes')
+const cors = require('cors')
+const path = require('path')
+const app = express();
+
+const _dirname = path.resolve()
+
+app.use(cors())
+
+app.use(express.json()) //if we know this line we'll get an error(undefined body)
+app.use(express.static(path.join(_dirname, '/Frontend/dist')));
+app.get('*', (_, res)=>{
+    res.sendFile(path.resolve(_dirname, 'Frontend', 'dist', 'index.html'))
+})
+
+app.get('/', (req,res)=>{
+    res.send('Hello World')
+})
+
+app.use('/ai', aiRoutes);
 
 
 app.listen(3000, ()=>{
     console.log("Server is running");
+
 })
